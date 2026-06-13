818438363ed25f997691b56f588ff5eeb6fc2d8f
sadiyasaiya36-netizen <sadiyasaiya36@gmail.com>
2026-06-14 00:57:31 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -82,10 +82,10 @@ router.get("/", async (req, res) => {
             );
 
             const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const mg = `🛑 *ZORO - MD වෙතට සාදරයෙන් පිළිගන්නෙමි* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://pin.it/36KqPH5FU",
+                url: "sandbox:/mnt/data/726495.png0",
               },
               caption: sid,
             });
